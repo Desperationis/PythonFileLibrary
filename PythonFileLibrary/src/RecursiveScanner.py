d019839497ea749b76dae34c5a71feb3f114eabb
Diego Contreras <smarttdiego@gmail.com>
2020-12-22 11:26:59 -0800

    Implement RecursiveScanner.py
    Implemented RecursiveScanner.py's functions and made sure they passed
    the tests defined in integration/RecursiveScannerTest.py.

--- PythonFileLibrary/src/RecursiveScanner.py
+++ PythonFileLibrary/src/RecursiveScanner.py
@@ -1,39 +1,62 @@
 import os
 
-"""
-    RecursiveScanner.py
-
-    Recursively scans a root directory for leaf nodes and lists the files.
-"""
 class RecursiveScanner:
-    def __init__(self, directory, whitelist):
-        # List of files' names.
-        self.files = []
+    """Recursively scans a directory for files of a specific extension."""
 
-        # List of extensions of the files wanted (.c, .h, .py, ect.)
-        self.whitelist = whitelist
+    def __init__(self, directory):
+        self._directory = directory
+    
+    def _Scan(self, directory: str, whitelist: [str], maxDepth: int = 0) -> [str]:
+        foundFiles = []
+        dirObjects = os.listdir(directory)
 
-        # Directory of the root folder.
-        self.directory = directory
+        # Recursively search files until depth == 1 or there are no more
+        # folders. 
+        for obj in dirObjects:
+            objDir = os.path.join(directory, obj)
 
-        self.Scan(directory)
+            if os.path.isfile(objDir):
+                _, extension = os.path.splitext(objDir)
+                if extension in whitelist or len(whitelist) == 0:
+                    foundFiles.append(objDir)
+            
+            elif os.path.isdir(objDir) and (maxDepth > 1 or maxDepth <= 0):
+                foundFiles.extend(self._Scan(objDir, whitelist, maxDepth - 1))
+    
+        return foundFiles
 
-    def GetFileExtension(self, file):
-        extensions = file.split('.')
-        return '.' + extensions[-1]
+    def Scan(self, whitelist: [str], maxDepth: int = 0) -> [str]:
+        """Scan for files with a specific extension in this directory. 
 
-    def Scan(self, directory):
-        for obj in os.listdir(directory):
-            # If it's a folder, keep searching. If it's a weird file, skip it.
-            if '.' not in obj:
-                try:
-                    self.Scan(os.path.join(directory, obj))
-                except:
-                    pass
+        Args:
+            whitelist: 
+                A list of strings that denote the types of extensions wanted, such
+                as ['.txt', '.mp3']. If the list is empty, no filter will be done
+                and every single file in the directory will be returned.  
 
-            else:
-                # If it's a file in our whitelist, make a FileScanner for it
-                for item in self.whitelist:
-                    if item == self.GetFileExtension(obj):
-                        self.files.append(os.path.join(directory, obj))
-                        break
+            maxDepth: 
+                Optional; Specifies the maximum depth that will be searched. E.x.
+                A depth of 1 means that ONLY the current directory will be searched
+                for files, while a depth of 2 means that the current directory and 
+                the next level below it will be searched for files. If this is 0 
+                (default) or below, the entire directory will be searched.
+        
+        Returns:
+            A list of directories (as strings) that share the common directory
+            you specificied in __init__.
+
+            E.x. 
+
+            RecursiveScanner("folder/anotherfolder/") would give files like
+
+            ["folder/anotherfolder/text.txt", "folder/anotherfolder/text.mp3"]
+
+            And RecursiveScanner("home/<user>/Desktop/folder/anotherfolder/") 
+            would give files like
+
+            ["home/<user>/Desktop/folder/anotherfolder/text.txt", 
+            "home/<user>/Desktop/folder/anotherfolder/text.mp3"]
+        """
+
+
+        return self._Scan(self._directory, whitelist, maxDepth)
